65330e246fde53cf1540de5dfcb261362b3e796c
Filippo Passeggieri <f.passeggieri@ccsitaly.aero>
2020-06-16 16:34:06 +0200

    Added nLog

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CoreNLogText;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace a_web_api
 {
@@ -26,6 +20,9 @@ namespace a_web_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSingleton<ILog, LogNLog>();  
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ lognLog.cs
@@ -0,0 +1,39 @@
+using NLog;  
+namespace CoreNLogText  
+{  
+    public interface ILog  
+    {  
+        void Information(string message);  
+        void Warning(string message);  
+        void Debug(string message);  
+        void Error(string message);  
+    }  
+    public class LogNLog : ILog  
+    {  
+        private static ILogger logger = LogManager.GetCurrentClassLogger();  
+   
+        public LogNLog()  
+        {  
+        }  
+   
+        public void Information(string message)  
+        {  
+            logger.Info(message);  
+        }  
+   
+        public void Warning(string message)  
+        {  
+            logger.Warn(message);  
+        }  
+   
+        public void Debug(string message)  
+        {  
+            logger.Debug(message);  
+        }  
+   
+        public void Error(string message)  
+        {  
+            logger.Error(message);  
+        }  
+    }  
+}  
