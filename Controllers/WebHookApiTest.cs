a7df68d4df8bd8b36acec45536c7dd5f14efb417
Filippo Passeggieri <f.passeggieri@ccsitaly.aero>
2020-06-16 15:33:17 +0200

    1st release

---
+++ Controllers/WebHookApiTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace WebHookApiTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebHookController : ControllerBase
+    {
+        private readonly ILogger<WebHookController> _logger;
+
+        public WebHookController(ILogger<WebHookController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpPost]
+        [Route("AWBNotification")]
+        public IActionResult AWBNotification([FromBody] Payload pl)
+        {
+            var incomingVar = pl;
+            if (incomingVar == null)
+                return BadRequest();
+            _logger.LogInformation($"Notification received for {pl.AWB} / {pl.Event}");
+            return Ok();
+        }
+       
+    
+        public class Payload
+        {
+            public string AWB { get; set; }
+            public string Event { get; set; }
+        }
+
+       }
+
+}
