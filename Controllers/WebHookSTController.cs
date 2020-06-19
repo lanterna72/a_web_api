115da36ea8ba6ef7f2943ddd4f95a2dd35cc7c6e
Filippo Passeggieri <f.passeggieri@cargostart.net>
2020-06-19 17:07:07 +0200

    various tweaks

--- Controllers/WebHookSTController.cs
+++ Controllers/WebHookSTController.cs
@@ -1,27 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using CoreNLogText;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace WebHookApiTest.Controllers
+namespace a_web_api.Controllers
 {
-    [ApiController]
     [Route("[controller]")]
-    public class WebHookController : ControllerBase
+    [ApiController]
+    public class WebHookSTController : ControllerBase
     {
         private readonly ILog _logger;
 
-        public WebHookController(ILog logger)
+        public WebHookSTController(ILog logger)
         {
             _logger = logger;
         }
 
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Hello World" };
+        }
 
+
+        // POST api/<WebHookSTController>
         [HttpPost]
-        [Route("AWBNotification")]
-        public IActionResult AWBNotification([FromBody] Payload pl)
+        public IActionResult Post([FromBody] Payload pl)
         {
             var incomingVar = pl;
             if (incomingVar == null)
@@ -29,14 +37,11 @@ namespace WebHookApiTest.Controllers
             _logger.Information($"Notification received for {pl.AWB} / {pl.Event}");
             return Ok();
         }
-       
-    
+
         public class Payload
         {
             public string AWB { get; set; }
             public string Event { get; set; }
         }
-
-       }
-
+    }
 }
